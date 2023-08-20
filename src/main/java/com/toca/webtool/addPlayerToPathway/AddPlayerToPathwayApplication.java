3d643c6226f579bef60addd13c95546d6967479e
Jacob Maxwell <jmaxwell@tocafootball.com>
2023-08-19 20:23:54 -0400

    Added a json save for any players that were unsuccessfully added to pathways as a log for manual insertion or investigation.

--- src/main/java/com/toca/webtool/addPlayerToPathway/AddPlayerToPathwayApplication.java
+++ src/main/java/com/toca/webtool/addPlayerToPathway/AddPlayerToPathwayApplication.java
@@ -1,11 +1,13 @@
 package com.toca.webtool.addPlayerToPathway;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.toca.webtool.addPlayerToPathway.model.Pathway;
 import com.toca.webtool.addPlayerToPathway.utility.AddPlayerToPathway;
 import com.toca.webtool.addPlayerToPathway.utility.ExcelParser;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
@@ -13,15 +15,28 @@ import java.util.List;
 public class AddPlayerToPathwayApplication {
 
 	public static void main(String[] args) throws IOException {
-//		Pathway pathway = new Pathway(100000102, "Beginner Intensive", -2147482215, 65);
-//		AddPlayerToPathway.addPlayer(1, pathway);
 		List<Pathway> pathways = ExcelParser.parseExcel();
-
 		pathways.remove(0);
+
 		System.out.println("Number of Records: " + pathways.size());
 
-		pathways.get(1140).getData();
+		int count = 1;
+		for(Pathway pathway : pathways){
+			System.out.print("\n" + count + ": ");
+			AddPlayerToPathway.addPlayer(count, pathway);
+			count++;
+		}
 
+		saveFailures(AddPlayerToPathway.getFailures());
+	}
+
+	public static void saveFailures(List<Pathway> failures) throws IOException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		String jsonData = objectMapper.writeValueAsString(failures);
+
+		FileWriter fileWriter = new FileWriter("C:\\Users\\Dr. Maxwell\\dev\\addPlayerToPathway\\files\\FailedPlayerPathways.json");
+		fileWriter.write(jsonData);
+		fileWriter.close();
 	}
 
 }
--- src/main/java/com/toca/webtool/addPlayerToPathway/utility/AddPlayerToPathway.java
+++ src/main/java/com/toca/webtool/addPlayerToPathway/utility/AddPlayerToPathway.java
@@ -11,11 +11,14 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddPlayerToPathway {
 
     private static String endpoint = "https://dev.tocawebtool.com/api/v2/addPlayerToPathway";
     private static String authentication = "eyJraWQiOiJnUDJodk84UVNOaGpCYl9SZldTVUROZUJmdkcwR0dPeXBibHZVMlpkOWVrIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULmVpaHk3ODlZTW9IVDRzTFRBT01MRkNYaEFBUExwVHcwbzRpLTRKbUtWcUkiLCJpc3MiOiJodHRwczovL2Rldi0zNTE3NzIub2t0YXByZXZpZXcuY29tL29hdXRoMi9hdXNiazU3NXplWFRPaUdpQjBoNyIsImF1ZCI6Imh0dHBzOi8vYXBpLnN0b3JtcGF0aC5jb20vdjEvYXBwbGljYXRpb25zLzJGYmF2Vm9UMjI0aG9EYzBNWDFzUE0iLCJpYXQiOjE2OTI0NjA0MzAsImV4cCI6MTY5MjU0NjgzMCwiY2lkIjoiMG9hYmp4dW52bDNwTHJ2MWwwaDciLCJ1aWQiOiIwMHViazU2NHJlVjQ2UDhWbjBoNyIsInNjcCI6WyJvcGVuaWQiXSwiYXV0aF90aW1lIjoxNjkyNDYwNDMwLCJzdWIiOiJzd2FwbmlsQGsyLWluYy5jb20ifQ.RAKgIcyTRab20prhGZzdao1vUIdgmXEOr6x00Jz5Bq4kMmVFwP3KEqvvNEPAENuGz1C-5ghelCrdyb2RNmF3kztfpZYkMIPyT-WOQ89rSnJoPEaoOgXGXv6ZC-JAkPY-K1mdZmtXglVhskk3hWRqJaq7ZPyXL9dyOErxCi1pq5kz-cua875YEg7AdmyRkQFN49OTnE1dw_edCFNHsOyZyraEDLeTGrdwbQayOElHV7EeyWv8WX8F7SqyCN9F6syDVVdB3UQUpQxXD4NoWc2U7qshsUqcICH8TGrgwEXijhCNPQOSY4EchylKWHd-jpI5vCuFu4z3COKGkxGUHPZWYA";
+    private static List<Pathway> failures = new ArrayList<>();
 
     public static void addPlayer(int record, Pathway pathway) throws IOException {
         Gson gson = new GsonBuilder().create();
@@ -45,11 +48,15 @@ public class AddPlayerToPathway {
             System.out.println(url.getPath());
             int i;
             while((i = bufferedReader.read()) != -1){
-                System.out.println((char) i);
+                System.out.print((char) i);
             }
+            failures.add(pathway);
         } else {
             System.out.println(record + ". Player Id: " + pathway.getPlayerId() + " with Site Id: " + pathway.getSiteId() + " was added to Pathway Id: " + pathway.getPathwayId() + " with Pathway Name: " + pathway.getPathwayName());
         }
     }
 
+    public static List<Pathway> getFailures() {
+        return failures;
+    }
 }
